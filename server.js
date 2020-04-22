4fb8e57590eca2e893ebc0c776c8480839cb09ec
Marcos Paulo O.P <marcos.poo@gmail.com>
2020-04-22 17:49:20 -0300

    Arquivos finais aula 1

---
+++ server.js
@@ -0,0 +1,32 @@
+const http = require('http');
+
+const servidor = http.createServer(function(req,resp){
+
+    let html = '';
+
+    if(req.url == '/'){
+        html = `<html>
+                <head>
+                    <meta charset="utf-8"> 
+                </head>
+                    <body>
+                        <h1>Casa do Código</h1>
+                    </body>
+                </html>`
+    }
+    else if(req.url == '/livros'){
+        html = `<html>
+                <head>
+                    <meta charset="utf-8"> 
+                </head>
+                    <body>
+                        <h1>Lista de Livros</h1>
+                    </body>
+                </html>`
+    }
+
+    resp.end(html);
+});
+
+servidor.listen(3000);
+
