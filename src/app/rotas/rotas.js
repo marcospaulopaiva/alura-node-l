f84e21da4d130e9740c77f5f8df196e4f468f83a
Marcos Paulo O.P <marcos.poo@gmail.com>
2020-04-29 11:36:05 -0300

    Arquivos finais aula 4

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -3,6 +3,7 @@ const db = require('../../config/database');
 
 
 module.exports = (app) => {
+    
     app.get('/', function(req, resp){
         resp.send(
             `<html>
@@ -17,9 +18,7 @@ module.exports = (app) => {
     });
     
     app.get('/livros', function(req, resp){
-
         const livroDao = new LivroDao(db);
-
         livroDao.lista()
                 .then(livros => resp.marko(
                     require('../views/livros/lista/lista.marko'),
@@ -27,6 +26,19 @@ module.exports = (app) => {
                         livros: livros
                     }
                 ))
+                .catch(erro => console.log(erro));
+    });
+
+    app.get('/livros/form', function(req, resp){
+        resp.marko(require('../views/livros/form/form.marko'));
+    });
+
+    app.post('/livros',function(req, resp){
+        console.log(req.body);
+        const livroDao = new LivroDao(db);
+
+        livroDao.adiciona(req.body)
+                .then(resp.redirect('/livros'))
                 .catch(erro => console.log(erro));
     });
 
